f81f0bd10466fb69a879ca9782da828567162142
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-08-19 22:19:01 +0900

    테스트용 병합버전(20.08.19)
    신필 프론트 + 정한 백엔드(게시판) 병합

---
+++ TeamProject/static/js/rooms.js
@@ -0,0 +1,22 @@
+var json_url = 'http://127.0.0.1:5000/board';
+fetch(json_url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+  	console.log(JSON.stringify(json));
+  	var text ='';
+    for (var i = 0; i <=json.length-1; i++) {
+	    var boards_html = 	
+	    			'<section class="board__lists__item">'+
+	    			'<h3>'+json[i].subject+'</h3>'+
+					'<p>'+json[i].content+'</p>' +
+					'<ul>'+
+						'<li>'+json[i].id+'</li>'+
+						'<li>'+json[i].create_date+'</li>'+
+					'</ul>'+'</section>';
+		text += boards_html;
+    }
+
+    document.querySelector('.board__lists').innerHTML = text;
+  });
