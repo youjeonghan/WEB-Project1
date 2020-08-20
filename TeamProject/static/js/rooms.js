24210cb050b516c34b56d125e32c05a7ec9ca943
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-08-20 19:37:44 +0900

    Update

--- TeamProject/static/js/rooms.js
+++ TeamProject/static/js/rooms.js
@@ -1,61 +1,53 @@
 const board_url = 'http://127.0.0.1:5000/board';
-const user_url = '';
 
-paint_board();
-console.log(asdf);
-// fetch(json_url)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(json) {
-//   	console.log(JSON.stringify(json));
-//   	var text ='';
-//     for (var i = 0; i <=json.length-1; i++) {
-// 	    var boards_html = 	
-// 	    			'<section class="board__lists__item">'+
-// 	    			'<h3>'+json[i].subject+'</h3>'+
-// 					'<p>'+json[i].content+'</p>' +
-// 					'<ul>'+
-// 						'<li>'+json[i].id+'</li>'+
-// 						'<li>'+json[i].create_date+'</li>'+
-// 					'</ul>'+'</section>';
-// 		text += boards_html;
-//     }
-
-//     document.querySelector('.board__lists').innerHTML = text;
-//   });
+//init function
+load_board();
 
 //통신을 통하여 해당 url 정보를 json화 해서 반환
-  function fetch_tojson(url){
-    return fetch(url).then(function(response) {
+function fetch_tojson(url){
+  return fetch(url).then(function(response) {
+    if(response.ok){
       return response.json();
-    });
+    }
+    else{
+      alert("HTTP-ERROR: " + response.status);
+    }
+  });
 
-  }
-// 비동기함수 async는 await가 완료될때 까지 대기후 실행, 게시글 조회
-  async function paint_board(){
-    //user_url변수를 통해 json형식의 user정보를 user변수에 저장
-    // const user = await fetch_tojson(user_url);
+}
+
+//게시판 글 태그 만들기 
+function paint_board(board){
+  const board_html =   
+  '<section class="board__lists__item">'+
+  '<h3>'+board.subject+'</h3>'+
+  '<p>'+board.content+'</p>' +
+  '<ul>'+
+  '<li>'+board.id+'</li>'+
+  '<li>'+board.create_date+'</li>'+
+  '</ul>'+'</section>'; 
+  return board_html;
+}
+
+// 게시글 조회, 비동기함수 async는 await가 완료될때 까지 대기후 실행
+async function load_board(){
     //board_url변수를 통해 json형식의 board정보를 boards변수에 저장
-    const boards = await fetch_tojson(board_url);
-    //게시판 tag 생성
-    const text ='';
-    for (var i = 0; i <=boards.length-1; i++) {
-      const boards_html =   
-              '<section class="board__lists__item">'+
-              '<h3>'+boards[i].subject+'</h3>'+
-            '<p>'+boards[i].content+'</p>' +
-            '<ul>'+
-              '<li>'+boards[i].id+'</li>'+
-              '<li>'+boards[i].create_date+'</li>'+
-            '</ul>'+'</section>';
-      //로그인한 유저정보 id 값과 게시글 id값이 같으면 수정 or 삭제가능 
-      // if(user.id === boards[i]){
-      //     //x 버튼과 수정 버튼 구현해서 태그에 저장
-      //     boards_html +='tags~';
-      // }
-      text += boards_html;
+    try{
+      const boards = await fetch_tojson(board_url);
+      //게시판 tag 생성
+      let text ='';
+      for (var i = boards.length-1; i >=0; i--) {
+        text += paint_board(boards[i]);
+      }
+      document.querySelector('.board__lists').innerHTML = text;
+    } catch(error){
+      console.log(error);
     }
 
-    document.querySelector('.board__lists').innerHTML = text;
   }
+
+
+function input_board(){
+// 입력창 박스를 만들고 , 엔터치면 이벤트헨들러로 데이터 전송, 페인트 함수 호출 
+
+}
--- TeamProject/teamproject.cmd
+++ TeamProject/teamproject.cmd
@@ -1,5 +1,5 @@
 @echo off
-@cd C:\coding\TeamProject
+@cd C:\Users\leesi\OneDrive\문서\GitHub\WEB-Project1\TeamProject
 @set FLASK_APP=app
 @set FLASK_ENV=development
-@C:\coding\TeamProject\teamproject\Scripts/activate
+@C:\Users\leesi\OneDrive\문서\GitHub\WEB-Project1\TeamProject\teamproject\Scripts/activate
