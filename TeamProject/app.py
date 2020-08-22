ab93b758af772beb4e1fd94a5f068197de0803b2
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-08-22 21:32:42 +0900

    삭제 수정(20.08.22)
    삭제안되는거 수정함

--- TeamProject/app.py
+++ TeamProject/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, jsonify, request
-# from models import db, Board
+from models import db, Board
 from flask import redirect
 from flask import render_template
 from datetime import datetime
@@ -9,40 +9,10 @@ import config
 
 app = Flask(__name__)
 app.config.from_object(config)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost:3306/flask_teamproject'
-db = SQLAlchemy(app)
 
 db.init_app(app)
 db.app = app
-db.create_all()
-
-class Board(db.Model):				# 게시판 모델 : id,제목,내용,생성시간
-	__tablename__ = 'noticeboard'
-	id = db.Column(db.Integer, primary_key=True)
-	subject = db.Column(db.String(200), nullable=False)
-	content = db.Column(db.Text(), nullable=False)
-	create_date = db.Column(db.DateTime(), nullable=False)
-
-	@property
-	def serialize(self):
-		return {
-			'id': self.id,
-			'subject': self.subject,
-			'content': self.content,
-			'create_date': self.create_date
-		}
-
-@app.route('/insert')
-def test():
-	for i in range(1,3):     
-		board = Board()
-		board.subject = "이신필 병신 %d" % i
-		board.content = "프론트 씨이발련아~ %d" % i
-		board.create_date = datetime.now()
-
-		db.session.add(board)
-		db.session.commit()
-	return "삽입 완료!!앙!"
+db.create_all()		# db를 초기화 해줌
 
 @app.route('/')
 def main():
@@ -90,7 +60,9 @@ def board_detail(id):
 		return jsonify(board.serialize)
 
 	elif request.method == 'DELETE':							# 삭제
-		Board.query.delete(Board.id == id)
+		board = Board.query.filter(Board.id == id).first()
+		db.session.delete(board)
+		db.session.commit()
 		return jsonify(), 204		# 204는 no contents를 의미한다(앞으로 이용할수 없다는 뜻을 명시적으로알림, 성공을 알리는거긴함)
 
 	data = request.get_json()
--- TeamProject/models.py
+++ TeamProject/models.py
@@ -1,20 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
 
-# from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
-# db = SQLAlchemy()
+class Board(db.Model):				# 게시판 모델 : id,제목,내용,생성시간
+    __tablename__ = 'noticeboard'
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
 
-# class Board(db.Model):				# 게시판 모델 : id,제목,내용,생성시간
-#     __tablename__ = 'noticeboard'
-#     id = db.Column(db.Integer, primary_key=True)
-#     subject = db.Column(db.String(200), nullable=False)
-#     content = db.Column(db.Text(), nullable=False)
-#     create_date = db.Column(db.DateTime(), nullable=False)
-
-#     @property
-#     def serialize(self):
-#         return {
-#             'id': self.id,
-#             'subject': self.subject,
-#             'content': self.content,
-#             'create_date': self.create_date
-#         }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'subject': self.subject,
+            'content': self.content,
+            'create_date': self.create_date
+        }
