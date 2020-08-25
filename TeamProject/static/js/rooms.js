f071e4d065fdf43ba4044174e67676494c331a65
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-08-25 22:47:33 +0900

    수정

--- TeamProject/static/js/rooms.js
+++ TeamProject/static/js/rooms.js
@@ -104,7 +104,21 @@ function hide_input(){
   ele.innerHTML = html;
 }
 
+//버튼 이벤트 헨들러
+async function handle_input(){
+  try{
+    const data = input_board();
+    await fetch_insert(data);
+    init();
+  } catch(error){
+    console.log(error);
+  }
 
+}
+
+////////보드 확대/////////
+// 보드 핸들러
+function handle_biginput(){
 //버튼 이벤트 헨들러Zxc v   
   const event_id = event.currentTarget.id.split('__');
   load_bigboard(event_id[1]);
